9af2d72881d5ca7e737cf5b71de69fd86b1e17bb
lyj <351675195@qq.com>
2024-03-19 00:20:01 +0800

    feat: 新增标记网关功能

--- src/components/baiduMap.vue
+++ src/components/baiduMap.vue
@@ -38,7 +38,6 @@ let map: any = null; // 保存 map 对象的全局变量
 // 地图标记点图标
 // 网关图标
 const GATEWAY = "src/components/icons/jian.png";
-const SIZE = [16, 16];
 // 传感器图标
 const SENSOR = "src/components/icons/point.png";
 // 车位图标
@@ -46,13 +45,6 @@ const SENSOR = "src/components/icons/point.png";
 // const SIZE2 = [32, 48]
 // 路口图标
 const CROSSING = "src/components/icons/green.png";
-
-// 标记用变量
-const all_sensor = ref([] as any);
-const pointArr = ref([] as any); //存放一条路的起点与终点
-const pts = ref([] as any); //存放一条路所有的传感器节点
-const pointIndex = ref(0); //初始化节点之间的距离
-const all = ref([] as any); //存放地图上所有标记的节点
 
 // 地图加载完成，拿到地图对象
 function ready({ BMap: BMapInstance, map: mapInstance }: any) {
@@ -68,8 +60,13 @@ function ready({ BMap: BMapInstance, map: mapInstance }: any) {
 }
 
 // 新增单个标记点
-function addMark(url: string, size: number[], position: number[]) {
-  let myIcon = new BMap.Icon(url, new BMap.Size(size[0], size[1]));
+function addMarkOnMap(url: string, size: number[], position: number[]) {
+  let myIcon;
+  if (url === "") {
+    myIcon = null;
+  } else {
+    myIcon = new BMap.Icon(url, new BMap.Size(size[0], size[1]));
+  }
   let point = new BMap.Point(position[0], position[1]);
   let marker = new BMap.Marker(point, {
     icon: myIcon,
@@ -77,65 +74,40 @@ function addMark(url: string, size: number[], position: number[]) {
   map.addOverlay(marker);
 }
 
+// 获取点距离
+function getDistance(start: number[], end: number[]) {
+  return map
+    .getDistance(
+      new BMap.Point(start[0], start[1]),
+      new BMap.Point(end[0], end[1]),
+    )
+    .toFixed(4);
+}
+
+// 清除标记
+function clear() {
+  map.clearOverlays();
+}
+
 // 向父组件传递事件
 const emit = defineEmits(["addMark"]);
-function callParent() {
-  // 向上传递事件
-  emit("addMark", "Hello world");
+
+function hw() {
+  console.log("Yes");
 }
 
-// 地图点击事件
+// 地图点击事件,传递给父组件
 function mapClick({ point }: any) {
-  pointArr.value.push(point);
-  // console.log(pointArr.value[0].lng)
-  const start = new BMap.Point(pointArr.value[0].lng, pointArr.value[0].lat);
-  let marker = new BMap.Marker(start);
-  map.addOverlay(marker);
-  // 如果点个数大于1
-  if (pointArr.value.length > 1) {
-    const end = new BMap.Point(pointArr.value[1].lng, pointArr.value[1].lat);
-    let marker = new BMap.Marker(end);
-    map.addOverlay(marker);
-    const x1 = start.lng;
-    const y1 = start.lat;
-    const x2 = end.lng;
-    const y2 = end.lat;
-    pointArr.value = [];
-    // 节点间距
-    pointIndex.value = 5;
-    const chang = map.getDistance(start, end).toFixed(4);
-    // console.log(chang)
-
-    //利用相似三角形求出所有点的坐标
-    for (let i = pointIndex.value; i < chang; i += pointIndex.value) {
-      let x3 = (i / chang) * (x1 - x2) + x2;
-      x3 = x3.toFixed(8);
-      let y3 = (i / chang) * (y1 - y2) + y2;
-      y3 = y3.toFixed(8);
-      let point1 = new BMap.Point(x3, y3);
-      pts.value.push(point1);
-    }
-
-    //二维数组，存放多条街道的坐标点
-    all_sensor.value.push(pts.value);
-    all.value.push(pts.value);
-    // console.log(all_sensor.value)
-
-    // 清除图上标记
-    map.clearOverlays();
-    // 在地图上标注这些点
-    for (let j = 0; j < all_sensor.value.length; j++) {
-      for (let k = 0; k < all_sensor.value[j].length; k++) {
-        addMark(SENSOR, SIZE, [
-          all_sensor.value[j][k].lng,
-          all_sensor.value[j][k].lat,
-        ]);
-        // console.log(all_sensor.value[j][k].lng, all_sensor.value[j][k].lat)
-        // marker0.setRotation(theta)
-      }
-    }
-  }
+  emit("addMark", point);
 }
+
+// 向外暴露方法
+defineExpose({
+  hw,
+  addMarkOnMap,
+  getDistance,
+  clear,
+});
 </script>
 
 <template>
@@ -159,18 +131,24 @@ function mapClick({ point }: any) {
     <!--    <bm-marker :position="{lng: 113.80848202641658, lat: 22.816506505348503}" :dragging="false" :icon=sIcon></bm-marker>-->
   </baidu-map>
   <el-button
-    @click="addMark(SENSOR, SIZE, [113.80848202641658, 22.816506505348503])"
+    @click="
+      addMarkOnMap(SENSOR, [16, 16], [113.80848202641658, 22.816506505348503])
+    "
     >传感器</el-button
   >
   <el-button
-    @click="addMark(GATEWAY, SIZE, [113.80868202641658, 22.816506505348503])"
+    @click="
+      addMarkOnMap(GATEWAY, [32, 32], [113.80868202641658, 22.816506505348503])
+    "
     >网关</el-button
   >
   <el-button
-    @click="addMark(CROSSING, SIZE, [113.80848202641658, 22.816906505348503])"
+    @click="
+      addMarkOnMap(CROSSING, [32, 32], [113.80848202641658, 22.816906505348503])
+    "
     >路口</el-button
   >
-  <el-button @click="callParent()">向父组件发送事件</el-button>
+  <!--  <el-button @click="callParent()">向父组件发送事件</el-button>-->
   <el-button @click="map.clearOverlays()">清空</el-button>
 </template>
 
--- src/views/addGateway.vue
+++ src/views/addGateway.vue
@@ -2,21 +2,85 @@
 import { ref } from "vue";
 import BaiduMap from "@/components/baiduMap.vue";
 
-// 标记用变量
-const all_sensor = ref([]);
-const pointArr = ref([]); //存放一条路的起点与终点
-const pts = ref([]); //存放一条路所有的传感器节点
-const str = ref([]);
-const pointIndex = ref(0); //初始化节点之间的距离
-const all = ref([]); //存放地图上所有标记的节点
+// 子组件实例
+interface MapRef {
+  hw: () => void;
+  addMarkOnMap: (url: string, size: number[], position: number[]) => void;
+  getDistance: (start: number[], end: number[]) => number;
+  clear: () => void;
+}
+const mapRef = ref<MapRef | null>(null);
 
-// 标记点
-function addMark() {}
+// 标记用变量
+const pointArr = ref([] as any); //存放一条路的起点与终点
+const pts = ref([] as any); //存放一条路所有的节点
+const pointIndex = ref(0); //初始化节点之间的距离
+const all = ref([] as any); //存放地图上所有标记的节点
+
+// 网关图标
+const GATEWAY = "src/components/icons/jian.png";
+// 路口图标
+const CROSSING = "src/components/icons/green.png";
+
+// 子组件点击地图时触发标注事件
+function addMark(point: any) {
+  pointArr.value.push(point);
+  mapRef.value?.addMarkOnMap(
+    "",
+    [32, 32],
+    [pointArr.value[0].lng, pointArr.value[0].lat],
+  );
+  // // 如果点个数大于1
+  if (pointArr.value.length > 1) {
+    // console.log(pointArr.value)
+    mapRef.value?.addMarkOnMap(
+      "",
+      [32, 32],
+      [pointArr.value[1].lng, pointArr.value[1].lat],
+    );
+    const x1 = pointArr.value[0].lng;
+    const y1 = pointArr.value[0].lat;
+    const x2 = pointArr.value[1].lng;
+    const y2 = pointArr.value[1].lat;
+    pointArr.value = [];
+    // 节点间距
+    pointIndex.value = 15;
+    let chang = mapRef.value?.getDistance([x1, y1], [x2, y2]);
+    // console.log(chang)
+    if (chang) {
+      //利用相似三角形求出所有点的坐标
+      for (let i = pointIndex.value; i < chang; i += pointIndex.value) {
+        let x3 = (i / chang) * (x1 - x2) + x2;
+        x3 = x3.toFixed(8);
+        let y3 = (i / chang) * (y1 - y2) + y2;
+        y3 = y3.toFixed(8);
+        pts.value.push([x3, y3]);
+      }
+    }
+
+    //二维数组，存放多条街道的坐标点
+    all.value.push(pts.value);
+    // console.log(all.value)
+
+    // 清除图上标记
+    mapRef.value?.clear();
+    // 在地图上标注这些点
+    for (let j = 0; j < all.value.length; j++) {
+      for (let k = 0; k < all.value[j].length; k++) {
+        mapRef.value?.addMarkOnMap(
+          GATEWAY,
+          [32, 32],
+          [all.value[j][k][0], all.value[j][k][1]],
+        );
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <!--  <div class="bm-view" style="background-color:#c51313;"></div>-->
-  <baidu-map></baidu-map>
+  <baidu-map ref="mapRef" @add-mark="addMark"></baidu-map>
 </template>
 
 <style></style>
